dcc9391ca6c9f3503ebb077bc662d8a74f68cdbf
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-07-28 10:07:45 -0700

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,12 @@
+Tuple1 = ()
+print("Initial empty Tuple: ")
+print(Tuple1)
+Tuple1 = ('manish', 'vivek')
+print("Tuple with the use of String: ")
+print(Tuple1)
+list1 = [1, 2, 4, 5, 6]
+print("Tuple using List: ")
+print(tuple(list1))
+Tuple1 = tuple('manish')
+print("Tuple with the use of function: ")
+print(Tuple1)
