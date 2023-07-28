818e3d7b33682e76e8bc341131fdab615ca52c18
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-07-28 10:07:21 -0700

    Add files via upload

---
+++ List.py
@@ -0,0 +1,12 @@
+List = []
+print("Blank List: ")
+print(List)
+List = [10, 20, 14]
+print("List of numbers: ")
+print(List)
+List = ["ram", "mohan", "shyam"]
+Strinput=input("Enter a string: ")
+List.append(Strinput)
+print("List Items with inputed string: ")
+for ele in List:
+    print(ele,end=",")
