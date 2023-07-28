f54108e12c210606bf12cdc5661cf554d3bfac96
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-07-28 10:07:08 -0700

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+String1 = "mynameismanishkumar"
+print("Initial String: ")
+print(String1)
+print("First character of String is: ",end=" ")
+print(String1[0])
+print("Last character of String is: ",end=" ")
+print(String1[-1])
+print("Reverse string with str[::-1]: ",end=" ")
+print(String1[::-1])
+print("Slicing characters from 3-12: ",end=" ")
+print(String1[3:12])
+print("Slicing characters between 3rd and 2nd last character: ",end=" ")
+print(String1[3:-2])
