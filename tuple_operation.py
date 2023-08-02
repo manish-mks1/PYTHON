dbb1a56b48ed857c52c95eab0b89b9f3fe44c059
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-08-02 11:29:09 -0700

    Add files via upload

---
+++ tuple_operation.py
@@ -0,0 +1,12 @@
+# creating tuples with college names..
+
+colleges = ("SIIET","BHARAT","GNIT", "AVN")
+
+print("the lists in colleges tuple is: ",colleges)
+
+print("we can\'t add or remove new elements in a tuple") 
+print("length of the tuple colleges is:",len(colleges))
+# checking whether 'SIIET' is present in the tuple or not
+
+if "SIIET" in colleges:
+    print("Yes, 'SIIET' is in the colleges tuple")
