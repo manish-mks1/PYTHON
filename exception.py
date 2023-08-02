db6c781165e1ac453b6faf51c513024d9d25ca8c
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-08-02 11:26:33 -0700

    Add files via upload

---
+++ exception.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+try:
+    c=a/b
+    print("Division=",c)
+except:
+    print("pgla gaye ho ka zero se divide hoga be...")
+else:
+    print("bohot sahi")
