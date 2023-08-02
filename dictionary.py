969819ab4aec767e66fa00ac7031101bc174bc96
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-08-02 11:26:17 -0700

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,35 +1,20 @@
-dict1={'apple':'fruits','age':23,'address':'noida'}
-z=1
-while(z!=0):
-    flag=0
-    print("**Dictionary***")
-    print("1. Search Word meaning")
-    print("2. Add word meaning")
-    print("Enter your choice:",end=" ")
-    n=int(input())
-    if n==1:
-        t= input("Type a word for search: ")
-        for x in dict1:
-            if x==t:
-                print(x," = ",dict1[x])
-                print("Thank You...")
-                flag=1
-        if(flag==0):
-            print("your word are not found")
-            print("please add this word to dictionary...")
-            print("Thank you...")
-    elif n==2:
-        print("******* Add word with meaning in dictionary **********")
-        print("Please Enter word with meaning by space seperated:",end=" ")
-        values=input()
-        list=values.split(" ")
-        dict1[list[0]]=list[1]
-        print("your word is add successfully...")
-    else:
-        print("invalid input")
-        print("please try again")
-    print("Do you want to continue y/n :",end=" ")
-    x=input()
-    if x=='n' or x=='N':
-        print("Thanks for Visiting my dictionary...")
-        z=0
+# creating a dictionary for SIIET 
+college = { "name": "siiet",
+            "code": "INDI",
+            "id": "x3"
+        }
+print(college)
+#adding items to dictionary 
+college["location"] = "IBP" 
+print(college)
+#changing values of a key 
+college["location"] = "sheriguda" 
+print(college)
+# to remove items use pop() 
+college.pop("code") 
+print(college)
+#know the length using len() 
+print("length of college is:",len(college)) 
+#to copy the same dictionary use copy() 
+mycollege= college.copy() 
+print(mycollege)
