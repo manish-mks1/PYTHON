98eb3dfde7f0b51e3451bca37a19dd55cd2d0e4d
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-08-02 11:28:50 -0700

    Add files via upload

---
+++ string_operation.py
@@ -0,0 +1,10 @@
+pi=3.14
+s= "Venkata"
+v= "Subhramanyam" 
+print("the value of s is:",s)
+print("the value of v is:",v) 
+string_add = s+v
+print("after concatenating s and v the string is:",s+v)
+
+text = 'The value of pi is ' + str(pi)
+print(text)
