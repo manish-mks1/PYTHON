1dd56baccb3ac4974d345cd1c3a1542c5d80e2bc
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-08-02 11:25:16 -0700

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+a=10; b=3
+
+print("addition of a:",a,"&b:",b,"is:",a+b)
+
+print("substraction of a:",a,"&b:",b,"is:",a-b)
+
+print("multiplication of a:",a,"&b:",b,"is:",a*b)
+
+print("division of a:",a,"&b:",b,"is:",a/b)
+
+print("floor divison of a:",a,"&b:",b,"is:",a//b)
+
+print("moduli of a:",a,"&b:",b,"is:",a%b)
+
+print("exponent of a:",a,"&b:",b,"is:",a**b)
