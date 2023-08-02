e5cc371637b3c045572b2cf78fe1a2316a24a1be
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2023-08-02 11:26:01 -0700

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+i=7 
+c=24+8j
+f=7.01
+s='HELLO EVERYONE!!\nThis is john\'s python programming..'
+b= True
+print("the value of integer is:",i,' its type is:',type(i)) 
+print("the value of float is:",f,' its type is:',type(f)) 
+print("the value of complex is:",c,' its type is:',type(c)) 
+print("the value of string is:",s,' its type is:',type(s)) 
+print("the value of boolean is:",b,' its type is:',type(b))
